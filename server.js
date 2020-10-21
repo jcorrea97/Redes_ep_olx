2f26257dd03b2efe0d3466474f0a916a148fb482
Kakaique2000 <kaique.lisboa@gmail.com>
2020-10-20 23:39:44 -0300

    sistema de conexao ws multi-telas e avaliacoes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const userService = require('./user');
 
 const app = express();
 const server = require('http').createServer(app);
@@ -10,6 +13,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+app.use(cookieParser("1234"));
+app.use(bodyParser.json())
 
 
 app.get('/home', (req, res) => {
@@ -29,17 +35,38 @@ app.get('/novoproduto', (req, res) => {
     res.render('novoproduto.html');
 });
 
+app.post('/login', (req, res) => {
+    const { name } = req.body
+   
+    res.cookie('userId', user.id)
+    res.json(user);
+})
+
+app.all("/**", (req, res) => {
+    res.render('notFound.html')
+})
+
 let messages = [];
 
 let produtos = [];
 
-var user =[];
+var user = [];
+
+sameConnectionPool = {};
 
 
 //socket.emit = mandar mensagem para o socket
 //socket.on = ouvir uma mensagem
 // socket.broadcast.emit = envia para todo mundo
 io.on('connection', socket =>{
+
+    // Sempre que houver uma conexão, ele vai pegar o campo token do handshake, e vai tentar meio que relogar o usuário
+    // A identificação é sempre pelo id do usuário, a gente recebe ele pelo cookie que chega do backend
+    // Esse ID que recebemos pelo cookie faz o socket.io meio que pegar essa conexão nova e assinalar a um usuário logado
+    // Permitindo por exemplo, o usuário que postou um produto ser identificado pelo seu ID, e em qualquer tela que estiver
+    // Dessa forma conseguimos mandar mensagem a um usuario onde quer que ele esteja através do seu ID gerenciado pelo UserService
+    userService.login('', socket.id, socket.handshake.query && socket.handshake.query.token)
+    
     console.log(`novo usuario conectado : ${socket.id}`);
     //id.push(socket.id);
     socket.emit('previousMessages', messages );
@@ -47,14 +74,13 @@ io.on('connection', socket =>{
     socket.emit('usuario', user);
 
     socket.on('initUser', data => {
-        user.push(data);
-        socket.emit('usuarioNovo', data);
+        const user = userService.login(data.name, socket.id, data.token)
+        socket.emit('usuarioNovo', user);
     });
 
     socket.on('comprarproduto', data => {
         produtos.pop(data);        
     });
-
 
     socket.on('sendMessage', data => {
         messages.push(data);
@@ -65,6 +91,16 @@ io.on('connection', socket =>{
         produtos.push(data);
         socket.broadcast.emit('receivedProdutos', data);
     });
+
+    socket.on('avaliaUsuario', data => {
+        userService.rateUser(data.token, data.rate, data.userRatedId)
+        userRated = userService.getUser(data.userRatedId);
+
+        console.log(userRated)
+
+        if(userRated)
+        io.to(userRated.socketId).emit('recebeAvaliacao', userRated.ratings);
+    })
 
 });
 
---
+++ user.js
@@ -0,0 +1,70 @@
+class User {
+
+    id;
+    name;
+    ratings = {
+        '0 estrelas': 0,
+        '1 estrelas': 0,
+        '2 estrelas': 0,
+        '3 estrelas': 0,
+        '4 estrelas': 0,
+        '5 estrelas': 0
+    };
+
+    constructor(name, socketId) {
+        this.id = User.generateId();
+        this.name = name
+        this.socketId = socketId;
+    }
+
+    addRating(rate, userRating) {
+        this.ratings[rate + ' estrelas']++;
+    }
+
+    static generateId() {
+        return Math.floor(Math.random() * 1000000000)
+    }
+
+}
+
+class UserService {
+
+    users = {}
+
+    constructor() {}
+
+    login(name, socketId, token) {
+
+        if(token && this.users[token]) {
+            this.users[token].socketId = socketId
+            console.log('reconexão de usuário, atualizando id do socket,\n usuario de id: ' + token + '\nrecebe o socket: ' + socketId)
+            return;
+        }
+
+        if(!name) {
+            console.log('sem nome e não é reconexão, saindo...')
+            return
+        }
+
+        const user = new User(name, socketId)
+
+        while(this.users[user.id]) {
+            user.id = User.generateId();
+        }
+
+        this.users[user.id] = user;
+        return user;
+    }
+
+    getUser(id) {
+        return this.users[id] || null
+    }
+
+    rateUser(userRatingId, rate, userRatedId) {
+        if(this.users[userRatedId])
+        this.users[userRatedId].addRating(rate, userRatingId);
+    }
+
+}
+
+module.exports = new UserService();
